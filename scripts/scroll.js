36a801c92c5ef6d1b5e53afb0340ca45b0e618a1
Kevin-Itam <kevinitamar16@gmail.com>
2024-03-26 20:37:34 -0300

    16/03/2024

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,14 +1,16 @@
-const navlinks = document.querySelectorAll('.nav__link');
-
-navlinks.forEach(navlinkEL => {
-    navlinkEL.addEventListener('click', () => {
-        document.querySelector('.active')?.classList.remove('active');
-        navlinkEL.classList.add('active');
-    });
-});
-
 function scrollElement(scroll) {
     var element = document.getElementById(scroll);
     element.scrollIntoView({ behavior: "smooth", block: "end", inline: "center" });
 
-};
+    // Atualiza a classe ativa do link correspondente ao elemento
+    const navlinks = document.querySelectorAll('.nav__link');
+    navlinks.forEach(navlinkEL => {
+        // Remove a classe ativa de todos os links
+        navlinkEL.classList.remove('active');
+
+        // Verifica se o href do link corresponde ao ID do elemento de rolagem
+        if (navlinkEL.getAttribute('href') === `#${scroll}`) {
+            navlinkEL.classList.add('active');
+        }
+    });
+}
