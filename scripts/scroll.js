416c3f4df626b8a9ab92b1253361c00b8a495b44
Kevin-Itam <kevinitamar16@gmail.com>
2024-04-01 20:51:52 -0300

    1/04/2024

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,22 +1,6 @@
-// function scrollElement(scroll) {
-//     var element = document.getElementById(scroll);
-//     element.scrollIntoView({ behavior: "smooth", block: "end", inline: "center" });
-
-//     // Atualiza a classe ativa do link correspondente ao elemento
-//     const navlinks = document.querySelectorAll('.nav__link');
-//     navlinks.forEach(navlinkEL => {
-//         // Remove a classe ativa de todos os links
-//         navlinkEL.classList.remove('active');
-
-//         // Verifica se o href do link corresponde ao ID do elemento de rolagem
-//         if (navlinkEL.getAttribute('href') === `#${scroll}`) {
-//             navlinkEL.classList.add('active');
-//         }
-//     });
-// }
 function scrollElement(scroll) {
     var element = document.getElementById(scroll);
-    element.scrollIntoView({ behavior: "smooth", block: "end", inline: "center" });
+    element.scrollIntoView({ behavior: "smooth", block: "start", inline: "center" });
 
     // Atualiza a classe ativa do link correspondente ao elemento
     updateActiveLink(scroll);
@@ -35,29 +19,3 @@ function updateActiveLink(scroll) {
     });
 }
 
-// Adiciona um ouvinte de evento de rolagem ao documento
-document.addEventListener('scroll', function() {
-    // Obtém a seção atualmente visível
-    const visibleSectionId = getCurrentSectionId();
-    
-    // Atualiza a classe ativa do link correspondente à seção visível
-    updateActiveLink(visibleSectionId);
-});
-
-// Função para obter o ID da seção atualmente visível
-function getCurrentSectionId() {
-    let currentSectionId = null;
-    const sections = document.querySelectorAll('section');
-    
-    sections.forEach(section => {
-        const rect = section.getBoundingClientRect();
-        const paddingTop = parseFloat(getComputedStyle(section).paddingTop);
-        const sectionTop = rect.top - paddingTop; // Ajuste para levar em conta o padding
-
-        if (sectionTop <= window.innerHeight && rect.bottom >= 0) {
-            currentSectionId = section.id;
-        }
-    });
-
-    return currentSectionId;
-}
